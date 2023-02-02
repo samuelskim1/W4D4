2dc80fef9313e7bbc38d415390c3711fb06d7e4b
Samuel Kim <samuelskim420@gmail.com>
2023-02-02 23:11:12 +0000

    sam changes

--- hanoi/lib/tower_of_hanoi.rb
+++ hanoi/lib/tower_of_hanoi.rb
@@ -1,11 +1,11 @@
 class Hanoi
     attr_reader :tower1, :tower2, :tower3
 
-    # def initialize
-    #     @tower1 = [5, 4, 3, 2, 1]
-    #     @tower2 = []
-    #     @tower3 = []
-    # end
+    def initialize
+        @tower1 = [5, 4, 3, 2, 1]
+        @tower2 = []
+        @tower3 = []
+    end
 
 
 
