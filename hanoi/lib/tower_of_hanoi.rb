204944dc624bb13445bc54c31ed3889c01a2db62
Samuel Kim <samuelskim420@gmail.com>
2023-02-02 23:04:54 +0000

    sam's work

---
+++ hanoi/lib/tower_of_hanoi.rb
@@ -0,0 +1,12 @@
+class Hanoi
+    attr_reader :tower1, :tower2, :tower3
+
+    # def initialize
+    #     @tower1 = [5, 4, 3, 2, 1]
+    #     @tower2 = []
+    #     @tower3 = []
+    # end
+
+
+
+end
--- hanoi/spec/tower_of_hanoi_spec.rb
+++ hanoi/spec/tower_of_hanoi_spec.rb
@@ -6,12 +6,15 @@ describe Hanoi do
 
     describe '#initialize' do
         it 'should initialize with three arrays representing towers' do
-            expect(game.tower1).to be_truthy
-            expect(game.tower2).to be_truthy
-            expect(game.tower3).to be_truthy
+            expect(game.tower1).to be_an_instance_of(Array)
+            expect(game.tower2).to eq([])
+            expect(game.tower3).to eq([])
         end
 
-        
+        it 'the first tower should have 5 discs in descending order starting with 5' do
+            expect(game.tower1).to eq([5,4,3,2,1])
+        end
+
 
     end
 
